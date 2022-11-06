ddba6e3ae9a0d89e37d3e4fa96ecbf0e850f92a5
Pavel Nikolaevskiy <pavel.nik@freeteam.ru>
2022-11-06 15:58:05 +0300

    работы с инпутом

---
+++ Assets/Scripts/ECS/Systems/InputSystem.cs
@@ -0,0 +1,44 @@
+﻿using FreeTeam.BubbleShooter.ECS.Components;
+using FreeTeam.BubbleShooter.Services;
+using Leopotam.EcsLite;
+using Leopotam.EcsLite.Di;
+using UnityEngine;
+
+namespace FreeTeam.BubbleShooter.ECS.Systems
+{
+    public sealed class InputSystem : IEcsRunSystem
+    {
+        #region Inject
+        private readonly EcsWorldInject world = default;
+
+        private readonly EcsPoolInject<InputHeld> inputHeldPool = default;
+        private readonly EcsPoolInject<InputReleased> inputReleasedPool = default;
+        private readonly EcsPoolInject<WorldPosition> worldPositionPool = default;
+
+        private readonly EcsCustomInject<ISceneContext> sceneContext = default;
+        #endregion
+
+        #region Implementation
+        public void Run(IEcsSystems systems)
+        {
+            int entity = -1;
+
+            if (Input.GetMouseButton(0))
+            {
+                entity = world.Value.NewEntity();
+                inputHeldPool.Value.Add(entity);
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                entity = world.Value.NewEntity();
+                inputReleasedPool.Value.Add(entity);
+                inputHeldPool.Value.Add(entity);
+            }
+
+            if (entity >= 0)
+                worldPositionPool.Value.Add(entity).Value = sceneContext.Value.Camera.ScreenToWorldPoint(Input.mousePosition);
+        }
+        #endregion
+    }
+}
