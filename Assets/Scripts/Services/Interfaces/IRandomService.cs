074f26eefe3dc0ac29bad036f117c2f25cf71b1c
Pavel Nikolaevskiy <pavel.nik@freeteam.ru>
2022-11-06 17:01:13 +0300

    добавил сервис для рандомизации

---
+++ Assets/Scripts/Services/Interfaces/IRandomService.cs
@@ -0,0 +1,8 @@
+﻿namespace FreeTeam.BubbleShooter.Services
+{
+    public interface IRandomService
+    {
+        int Range(int min, int max);
+        float Range(float min, float max);
+    }
+}
---
+++ Assets/Scripts/Services/RandomService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FreeTeam.BubbleShooter.Services
+{
+    public sealed class RandomService : IRandomService
+    {
+        #region Private
+        private readonly Random _rng;
+        #endregion
+
+        public RandomService(int? seed)
+        {
+            _rng = seed.HasValue ? new Random(seed.Value) : new Random();
+        }
+
+        #region Public methods
+        public int Range(int min, int max) =>
+            _rng.Next(min, max);
+
+        public float Range(float min, float max) =>
+            min + (max - min) * (float)_rng.NextDouble();
+        #endregion
+    }
+}
