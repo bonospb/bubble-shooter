33b220b9db1808e1d6e04146de5c5e06474e991d
Pavel Nikolaevskiy <pavel.nik@freeteam.ru>
2022-11-06 15:56:16 +0300

    обновление отображения траектории

---
+++ Assets/Scripts/ECS/Systems/TrajectoryViewUpdateSystem.cs
@@ -0,0 +1,35 @@
+﻿using FreeTeam.BubbleShooter.ECS.Components;
+using FreeTeam.BubbleShooter.Services;
+using Leopotam.EcsLite;
+using Leopotam.EcsLite.Di;
+
+namespace FreeTeam.BubbleShooter.ECS.Systems
+{
+    public sealed class TrajectoryViewUpdateSystem : IEcsRunSystem
+    {
+        #region Inject
+        private readonly EcsFilterInject<Inc<Trajectory, WorldPosition>> trajectoryFilter = default;
+
+        private readonly EcsPoolInject<WorldPosition> worldPositionPool = default;
+
+        private readonly EcsCustomInject<ISceneContext> sceneContext = default;
+        #endregion
+
+        #region Implementation
+        public void Run(IEcsSystems systems)
+        {
+            var trajectoryCount = trajectoryFilter.Value.GetEntitiesCount();
+
+            sceneContext.Value.TrajectoryRenderer.enabled = !trajectoryFilter.Value.IsEmpty();
+            sceneContext.Value.TrajectoryRenderer.positionCount = trajectoryCount;
+
+            var i = 0;
+            foreach (var entity in trajectoryFilter.Value)
+            {
+                sceneContext.Value.TrajectoryRenderer.SetPosition(i, worldPositionPool.Value.Get(entity).Value);
+                i++;
+            }
+        }
+        #endregion
+    }
+}
