e49e6fc74d989181f5fea40b29a172e32dc5a031
Pavel Nikolaevskiy <pavel.nik@freeteam.ru>
2022-11-06 15:56:49 +0300

    очистка данных (и самих сущностей) траектории

---
+++ Assets/Scripts/ECS/Systems/TrajectoryClearSystem.cs
@@ -0,0 +1,23 @@
+﻿using FreeTeam.BubbleShooter.ECS.Components;
+using Leopotam.EcsLite;
+using Leopotam.EcsLite.Di;
+
+namespace FreeTeam.BubbleShooter.ECS.Systems
+{
+    public sealed class TrajectoryClearSystem : IEcsRunSystem
+    {
+        #region Inject
+        private readonly EcsWorldInject world = default;
+
+        private readonly EcsFilterInject<Inc<Trajectory>> _trajectoryFilter = default;
+        #endregion
+
+        #region Implementation
+        public void Run(IEcsSystems systems)
+        {
+            foreach (var entity in _trajectoryFilter.Value)
+                world.Value.DelEntity(entity);
+        }
+        #endregion
+    }
+}
