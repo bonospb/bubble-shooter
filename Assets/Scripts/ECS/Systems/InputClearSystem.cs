b08b580fb94621cedeb40614c9f807ca25dd3b3e
Pavel Nikolaevskiy <pavel.nik@freeteam.ru>
2022-11-06 15:57:45 +0300

    очистка данных (и самих сущностей) для работы с инпутом

---
+++ Assets/Scripts/ECS/Systems/InputClearSystem.cs
@@ -0,0 +1,27 @@
+﻿using FreeTeam.BubbleShooter.ECS.Components;
+using Leopotam.EcsLite;
+using Leopotam.EcsLite.Di;
+
+namespace FreeTeam.BubbleShooter.ECS.Systems
+{
+    public sealed class InputClearSystem : IEcsRunSystem
+    {
+        #region Private
+        private readonly EcsWorldInject world = default;
+
+        private readonly EcsFilterInject<Inc<InputHeld>> heldInputFilter = default;
+        private readonly EcsFilterInject<Inc<InputReleased>> releasedInputFilter = default;
+        #endregion
+
+        #region Implements
+        public void Run(IEcsSystems systems)
+        {
+            foreach (var entity in heldInputFilter.Value)
+                world.Value.DelEntity(entity);
+
+            foreach (var entity in releasedInputFilter.Value)
+                world.Value.DelEntity(entity);
+        }
+        #endregion
+    }
+}
